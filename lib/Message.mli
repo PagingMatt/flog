8598153c2432943140288dd6977708281683c1fe
Matt Harrison <matt.anthony.harrison@gmail.com>
2018-10-07 20:49:29 +0100

    Adds comments onto sig

--- lib/Message.mli
+++ lib/Message.mli
@@ -1,8 +1,12 @@
 open Unix
 
+(** Message module encapsulates types and a constructor needed for log
+    messages. *)
 module Message : sig
+  (** Message type. *)
   type t
 
+  (** Logging levels. *)
   type level =
     | Trace
     | Debug
@@ -11,5 +15,7 @@ module Message : sig
     | Error
     | Fatal
 
+  (** Constructor for the Message.t type. If no time is passed in then the
+      current UTC time when called will be stamped onto the message. *)
   val make : Level -> tm option -> string -> t
 end
