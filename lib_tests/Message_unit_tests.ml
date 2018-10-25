78970b4dcae1b09b713c6550998da0a7dbea3ebb
Matt A Harrison <matt.anthony.harrison@gmail.com>
2018-10-25 23:29:33 +0100

    Adds test for time serialization

--- lib_tests/Message_unit_tests.ml
+++ lib_tests/Message_unit_tests.ml
@@ -22,8 +22,14 @@ let cmp_gt_negative () =
   Alcotest.(check int) "Checks that when cmp_tm is applied to a newer message and an older message the result is negative."
       (-1) (cmp_tm a b)
 
+let string_of_message_time_serialized () =
+  let a = make (Some tm_a) Trace "Message A." |> string_of_message in
+  Alcotest.(check string) "Checks that prefix of serialized message is correct time in expected format."
+    "2018-01-01T00:00:01Z" (String.sub a 0 20)
+
 let unit_tests = [
-  ("Checks that when cmp_tm is applied to messages with equal time, zero is returned."                 , `Quick, cmp_equal_zero );
-  ("Checks that when cmp_tm is applied to an older message and a newer message the result is positive.", `Quick, cmp_lt_positive);
-  ("Checks that when cmp_tm is applied to a newer message and an older message the result is negative.", `Quick, cmp_gt_negative);
+  ("Checks that when cmp_tm is applied to messages with equal time, zero is returned."                 , `Quick, cmp_equal_zero                   );
+  ("Checks that when cmp_tm is applied to an older message and a newer message the result is positive.", `Quick, cmp_lt_positive                  );
+  ("Checks that when cmp_tm is applied to a newer message and an older message the result is negative.", `Quick, cmp_gt_negative                  );
+  ("Checks that prefix of serialized message is correct time in expected format.",                       `Quick, string_of_message_time_serialized);
 ]
